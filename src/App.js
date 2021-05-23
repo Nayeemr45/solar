2d316c49b87b2ab78e66c5b129c557ab4fceeb04
Nayeem Ahmed <45434391+Nayeemr45@users.noreply.github.com>
2021-05-23 15:19:05 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import HomaPage from './Pages/HomaPage';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomaPage />
-    </div>
+{/*       <HomaPage />
+ */}    
+ <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+ </div>
   );
 }
 
