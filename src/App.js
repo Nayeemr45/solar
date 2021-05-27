4aaab781bfcdab166f4b7f3a70f08d9f08858888
Nayeem Ahmed <45434391+Nayeemr45@users.noreply.github.com>
2021-05-27 15:38:31 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import HomaPage from './Pages/HomaPage';
-import Button from '@material-ui/core/Button';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HomaPage />
-    
- {/* <Button variant="contained">Default</Button>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button> */}
- </div>
+      <Footer />
+    </div>
   );
 }
 
