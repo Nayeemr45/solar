c18da2812089b664de26c8eeda15f57c7e6506ba
Nayeem Ahmed <45434391+Nayeemr45@users.noreply.github.com>
2021-05-27 18:29:03 +0600

    update

--- src/components/SolarCalculatorForm.js
+++ src/components/SolarCalculatorForm.js
@@ -9,11 +9,25 @@ import {
 
 function SolarCalculatorForm() {
   return (
-    <div>
+    <div className="solar-calculator-form">
       <FormControl>
         <InputLabel htmlFor="my-input">Email address</InputLabel>
         <Input id="my-input" aria-describedby="my-helper-text" />
         <FormHelperText id="my-helper-text">
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
+          We'll never share your email.
           We'll never share your email.
         </FormHelperText>
       </FormControl>
