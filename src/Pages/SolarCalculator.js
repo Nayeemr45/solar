d8bbee3d11346405bac739e28d2bacce8b13bb18
ashikahmedfahim <ashikahmed96@gmail.com>
2021-05-29 16:34:20 +0600

    email and telephone validation added

--- src/Pages/SolarCalculator.js
+++ src/Pages/SolarCalculator.js
@@ -8,6 +8,7 @@ import {
 } from "../Assets/data/SolarCalculatorData";
 import "../Assets/Css/Solar_Calculator.css";
 import { Box, Grid } from "@material-ui/core";
+import { People } from "@material-ui/icons";
 
 function SolarCalculator() {
   const [SolarData, setSolarData] = useState(SolarCalculatorData);
@@ -15,12 +16,31 @@ function SolarCalculator() {
   const [index, setIndex] = useState(0);
 
   const getUserData = (data) => {
-    data ? setUserData(userData.concat(data)) : console.log(data);
+    data ? setUserData(userData.concat(data)) : console.log('');
     setIndex(index + 1);
   };
 
   useEffect(() => {
-    console.log(userData);
+    if (index > 6) {
+      let finalUserData = {
+        People: userData[0].text,
+        eConsumption: userData[1].text,
+        time: userData[2].text,
+        slope: userData[3].text,
+        roofArea: userData[4].text,
+        direction: userData[5].text,
+        gender: userData[6].gender,
+        email: userData[6].email,
+        firstName: userData[6].firstName,
+        surName: userData[6].surName,
+        houseNo: userData[6].houseNo,
+        street: userData[6].street,
+        telephone: userData[6].telephone,
+        zip: userData[6].zip,
+      };
+      let finalUserDataJSON = userData ? JSON.stringify(finalUserData) : '';
+      console.log(finalUserDataJSON);
+    } 
   }, [userData]);
 
   return (
