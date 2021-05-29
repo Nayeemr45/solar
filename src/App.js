e0dc6bbf92948e4aa6daf139e7b9c2d530fc7256
ashikahmedfahim <ashikahmed96@gmail.com>
2021-05-29 14:13:49 +0600

    contact deleted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import HomaPage from './Pages/HomaPage';
-import Footer from './components/Footer';
-import SolarCalculator from './Pages/SolarCalculator';
-import ContactUs from './Pages/ContactUs';
-import {Route, Link} from 'react-router-dom';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HomaPage from "./Pages/HomaPage";
+import Footer from "./components/Footer";
+import SolarCalculator from "./Pages/SolarCalculator";
+import { Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Route path={'/'} exact>
+      <Route path={"/"} exact>
         <HomaPage />
       </Route>
-      <Route path={'/SolarCalculator'} exact>
+      <Route path={"/SolarCalculator"} exact>
         <SolarCalculator />
       </Route>
-      <Route path={'/Contact'} exact>
-        <ContactUs />
-      </Route>
+      <Route
+        path="/Contact"
+        component={() => {
+          window.location.href = "";
+          return null;
+        }}
+      />
       <Footer />
     </div>
   );
